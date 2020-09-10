131f2717213f8d51d5fa9ea9071e6ed8fd857354
Olga <soloviovaolga@rambler.ru>
2020-09-10 19:34:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var myName = prompt('Как вас зовут?');
+var mySurName = prompt("Какая у вас фамилия?");
+var myBirthYear = prompt("В каком году вы родились?");
+
+var currentYear = 2020;
+var age = currentYear - myBirthYear;
+
+alert ('Привет, ' + myName + ' ' + mySurName + '!');
+
+/*if (age < 20){
+  alert ("jhdf");
+}
+else if (age >= 20 && age < 40)
+{
+  alert("ieuwr");
+}
+else {
+  alert ('Привет, ' + myName + ' ' + mySurName + '!');
+}*/
